50392924f06dedef7deb9f0b1f8c6f0cd48aab2d
yesolje <yesolje@github.com>
2022-01-25 17:38:37 +0900

    20220125 commit

--- mouse.js
+++ mouse.js
@@ -26,23 +26,4 @@ function moveImg() {
 document.onmousemove = getMousePosition; // 마우스가 움직이면 getMousePosition 함수 실행
 setInterval("moveImg()", 30); //계속 반복하여 tail이 circle 좌표와 같아질때 까지
 
-//go 버튼 클릭 시 난이도 정보를 가지고 다음 페이지로 이동
-const goButton = document.querySelector("#goButton");
-goButton.addEventListener("click", function (event) {
-  if (levelstatus == 1) {
-    location.href = "minesweeper.html?row=9&column=9&mine=10";
-  } else if (levelstatus == 2) {
-    location.href = "minesweeper.html?row=16&column=16&mine=40";
-  } else if (levelstatus == 3) {
-    let row = document.querySelector("#customRow").value;
-    let column = document.querySelector("#customColumn").value;
-    let mine = document.querySelector("#customMine").value;
-    if (row == "" || column == "" || mine == "") {
-      alert("custom 모드는 가로,세로,지뢰 수를 설정해야 시작할 수 있습니다!");
-      return;
-    } else {
-      location.href =
-        "minesweeper.html?row=" + row + "&column=" + column + "&mine=" + mine;
-    }
-  }
-});
+
